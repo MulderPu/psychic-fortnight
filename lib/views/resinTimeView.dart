767c69ec82b0ae76471dd67d5cbba2a7863e5824
Mulder Pu <mulderpu@gmail.com>
2021-03-20 20:11:49 +0800

    Added validation to textfield

--- lib/views/resinTimeView.dart
+++ lib/views/resinTimeView.dart
@@ -15,6 +15,9 @@ class _ResinTimeViewState extends State<ResinTimeView> {
   var expectedTime;
   final regenerateTime = 8; // mins
 
+  // Create a global key that uniquely identifies the Form widget
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -24,85 +27,138 @@ class _ResinTimeViewState extends State<ResinTimeView> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Resin Time"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: yourResinController,
-                decoration: InputDecoration(
-                    labelText: 'Your Resin:',
-                    border: InputBorder.none,
-                    hintText: ''),
-              ),
-              TextField(
-                controller: resinNeededController,
-                decoration: InputDecoration(
-                    labelText: 'Resin Needed:',
-                    border: InputBorder.none,
-                    hintText: ''),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      onPrimary: Colors.white,
-                      primary: Colors.purple,
-                      onSurface: Colors.grey,
-                      minimumSize: Size(double.infinity, 50),
-                      elevation: 10,
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0),
-                      ),
-                    ),
-                    onPressed: () {
-                      // dismiss keyboard
-                      FocusScope.of(context).unfocus();
-
-                      int neededResin = int.parse(resinNeededController.text);
-                      int currentResin = int.parse(yourResinController.text);
-                      var remainResin = neededResin - currentResin;
-                      var totalMinutes = (remainResin * regenerateTime);
-                      var convertHourMins = totalMinutes / 60;
-                      var hours = convertHourMins.toInt();
-                      var mins = (convertHourMins - hours.toDouble()) * 60;
-                      
-                      setState(() {
-                        result = "$hours hours and ${mins.round()} mins";
-                        resinNeededController.text = neededResin.toString();
-                        yourResinController.text = currentResin.toString();
-
-                        var now = DateTime.now();
-                        var expectDatetime = now
-                            .add(Duration(hours: hours, minutes: mins.toInt()));
-
-                        expectedTime = DateFormat('yyyy-MM-dd – kk:mm')
-                            .format(expectDatetime);
-                      });
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: TextFormField(
+                    controller: yourResinController,
+                    cursorColor: Colors.purple,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Your Resin:',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Colors.purple, width: 2.0),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        hintText: ''),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Resin field cannot be empty.';
+                      }
+                      return null;
                     },
-                    child: Text('Calculate')),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Result: $result',
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Expected DateTime: $expectedTime',
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: TextFormField(
+                    controller: resinNeededController,
+                    cursorColor: Colors.purple,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Resin Needed:',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Colors.purple, width: 2.0),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        hintText: ''),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Resin needed field cannot be empty.';
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          onPrimary: Colors.white,
+                          primary: Colors.purple,
+                          onSurface: Colors.grey,
+                          minimumSize: Size(double.infinity, 50),
+                          elevation: 10,
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          // dismiss keyboard
+                          FocusScope.of(context).unfocus();
+
+                          if (_formKey.currentState.validate()) {
+                            int neededResin =
+                                int.parse(resinNeededController.text);
+                            int currentResin =
+                                int.parse(yourResinController.text);
+                            var remainResin = neededResin - currentResin;
+                            var totalMinutes = (remainResin * regenerateTime);
+                            var convertHourMins = totalMinutes / 60;
+                            var hours = convertHourMins.toInt();
+                            var mins = (convertHourMins - hours.toDouble()) * 60;
+
+                            setState(() {
+                              result = "$hours hours and ${mins.round()} mins";
+                              resinNeededController.text = neededResin.toString();
+                              yourResinController.text = currentResin.toString();
+
+                              var now = DateTime.now();
+                              var expectDatetime = now.add(
+                                  Duration(hours: hours, minutes: mins.toInt()));
+
+                              expectedTime = DateFormat('yyyy-MM-dd – kk:mm')
+                                  .format(expectDatetime);
+                            });
+                          }
+                        },
+                        child: Text('Calculate')),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Result: $result',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Expected DateTime: $expectedTime',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
